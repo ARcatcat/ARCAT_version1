dc7a516647eab974f8b17fb87569b74b1fd66ee8
June <sunny940704247@qq.com>
2018-05-20 21:44:52 +0800

    v1

---
+++ Assets/back_to_SceneOne.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;//注意这个不能少 
+using UnityEngine.SceneManagement;
+public class back_to_SceneOne : MonoBehaviour
+{
+
+
+    // Use this for initialization  
+    void Start()
+    {
+
+        GameObject btnObj = GameObject.Find("back_button");//"Button"为你的Button的名称  
+        Button btn = btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(delegate ()
+        {
+            this.GoNextScene(btnObj);
+        });
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+    }
+
+    public void GoNextScene(GameObject NScene)
+    {
+        SceneManager.LoadScene(1); //加载Index = 1 ，第1个场景
+    }
+}
---
+++ Assets/houseone.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class houseone : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    // Material mat;
+    Vector3 des;
+    bool hasbeenchanged = false;
+    float staY = 0.005f;
+
+    public Animator Cat;
+
+    Quaternion q = Quaternion.identity;
+
+    void transPosandDes()
+    {
+        Vector3 src = transform.GetChild(1).localPosition;
+        Vector3 rotateVector = des - src;
+        rotateVector.y = 0;
+        q = Quaternion.LookRotation(rotateVector);
+        transform.GetChild(1).rotation = q;
+    }
+
+    //按钮按下
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        string str = vb.VirtualButtonName;
+        string num = str.Substring(14, str.Length - 14);
+        int buttonNum = int.Parse(num);
+
+        hasbeenchanged = false;
+        Cat.SetBool("walk", true);
+        des = transform.GetChild(buttonNum + 1).localPosition; //将当前vb位置设置为终点位置
+        des.y = staY;
+        Vector3 world_des = transform.GetChild(buttonNum + 1).position;
+        world_des.y = transform.GetChild(0).position.y;
+        transform.GetChild(0).GetComponent<NavMeshAgent>().destination = world_des;
+
+        //mat.color = Color.red;
+
+        GameObject pfb = Resources.Load("Hit_01") as GameObject;
+        GameObject prefabInstance = Instantiate(pfb);
+        prefabInstance.transform.parent = transform.GetChild(buttonNum + 1);
+        prefabInstance.transform.localPosition = new Vector3(0, 0, 0);
+        prefabInstance.transform.localScale = new Vector3(1, 0.001f, 1);
+        Destroy(prefabInstance, 1f);
+
+        if (buttonNum == 17) //eat
+        {
+            Cat.SetBool("walk_to_eat", true);
+            Cat.SetBool("eat_to_idle", true);
+            Cat.SetBool("walk_to_jump", false);
+            Cat.SetBool("walk_to_sound", false);
+        }
+        else if (buttonNum == 18) //jump
+        {
+            Cat.SetBool("walk_to_jump", true);
+            Cat.SetBool("jump_to_idle", true);
+            Cat.SetBool("walk_to_eat", false);
+            Cat.SetBool("walk_to_sound", false);
+        }
+        else if (buttonNum == 19) //sound
+        {
+            Cat.SetBool("walk_to_sound", true);
+            Cat.SetBool("sound_to_idle", true);
+            Cat.SetBool("walk_to_jump", false);
+            Cat.SetBool("walk_to_eat", false);
+        }
+        else
+        {
+            Cat.SetBool("walk_to_eat", false);
+            Cat.SetBool("walk_to_jump", false);
+            Cat.SetBool("walk_to_sound", false);
+        }
+
+
+    }
+
+
+    //按钮释放
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //  mat.color = Color.yellow;
+    }
+
+    void Start()
+    {
+
+        // mat = transform.GetChild(0).GetComponent<MeshRenderer>().material;
+
+        des = transform.GetChild(1).localPosition;
+        des = new Vector3(des.x, des.y, des.z);
+
+        //注册所有的vb事件
+        VirtualButtonBehaviour[] vbBehaviours = this.GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbBehaviours.Length; i++)
+        {
+            vbBehaviours[i].RegisterEventHandler(this);
+        }
+    }
+    void Update()
+    {
+        transform.GetChild(1).localPosition = new Vector3(transform.GetChild(0).localPosition.x, staY, transform.GetChild(0).localPosition.z);
+        //same
+        if (transform.GetChild(1).localPosition == des)
+        {
+            Cat.SetBool("walk", false);
+            Cat.SetBool("walk_to_idle", true);
+            //    mat.color = Color.black;
+            if (hasbeenchanged == false)
+            {
+                transform.GetChild(1).localEulerAngles = new Vector3(0, -180, 0);
+                hasbeenchanged = true;
+            }
+        }
+
+        else
+        {
+            transPosandDes();
+            //transform.GetChild(1).localPosition = Vector3.MoveTowards(transform.GetChild(1).localPosition, des, 0.1f * Time.deltaTime);
+        }
+    }
+
+    public void GoNextScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
